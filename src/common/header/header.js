52723b92fe8fc0c7573a1bf2a8ab976ccc72d3d1
Sehbaz Rafik <sehbazm@gmail.com>
2021-04-09 07:01:30 +0100

    header component with logo added

--- src/common/header/header.js
+++ src/common/header/header.js
@@ -1,11 +1,29 @@
 import React from "react";
-
+import { makeStyles } from "@material-ui/core/styles";
+import { withStyles } from "@material-ui/core/styles";
 import "../header/header.css";
 const { Component } = require("react");
-
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+  },
+}));
 class Header extends Component {
   render() {
-    return <div>Heading Component</div>;
+    const { classes } = this.props;
+
+    return (
+      <div className="header-container">
+        <div className="container-item logo">Image Viewer</div>
+        <div className="container-item"> Logo</div>
+      </div>
+    );
   }
 }
-export default Header;
+export default withStyles(useStyles)(Header);
