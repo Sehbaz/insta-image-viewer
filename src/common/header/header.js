0781ad9d8da1f2346b04542282c50fa31f91356d
Sehbaz Rafik <sehbazm@gmail.com>
2021-04-09 06:25:01 +0100

    Header Component skeleton code added

---
+++ src/common/header/header.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+import "../header/header.css";
+const { Component } = require("react");
+
+class Header extends Component {
+  render() {
+    return <div>Heading Component</div>;
+  }
+}
+export default Header;
--- src/screens/login/login.js
+++ src/screens/login/login.js
@@ -1,11 +1,17 @@
 import React from "react";
-
 import "../login/login.css";
+import Header from "../../common/header/header";
+
 const { Component } = require("react");
 
 class Login extends Component {
   render() {
-    return <div>Login Page</div>;
+    return (
+      <div>
+        <Header />
+        Login Page
+      </div>
+    );
   }
 }
 export default Login;
