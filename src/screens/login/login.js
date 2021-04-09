a3d1990bd5b484b5cf1e5861b0195bc026e15b20
Sehbaz Rafik <sehbazm@gmail.com>
2021-04-09 06:21:21 +0100

    login page skeleton code added

---
+++ src/screens/login/login.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+import "../login/login.css";
+const { Component } = require("react");
+
+class Login extends Component {
+  render() {
+    return <div>Login Page</div>;
+  }
+}
+export default Login;
