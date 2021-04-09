586d94c225364e5ad654bafef5729bf8b5dbb1b0
Sehbaz Rafik <sehbazm@gmail.com>
2021-04-09 10:02:25 +0100

    login form validation added

--- src/screens/login/login.js
+++ src/screens/login/login.js
@@ -7,8 +7,38 @@ import Card from "@material-ui/core/Card";
 import CardContent from "@material-ui/core/CardContent";
 import InputLabel from "@material-ui/core/InputLabel";
 import Input from "@material-ui/core/Input";
+import { FormHelperText } from "@material-ui/core/";
 const { Component } = require("react");
+
 class Login extends Component {
+  constructor() {
+    super();
+    this.state = {
+      username: "",
+      usernameRequired: "dispNone",
+      password: "",
+      passwordRequired: "dispNone",
+    };
+  }
+  loginHandler = () => {
+    if (this.state.username === "") {
+      this.setState({ usernameRequired: "dispBlock" });
+    } else {
+      this.setState({ usernameRequired: "dispNone" });
+    }
+    if (this.state.password === "") {
+      this.setState({ passwordRequired: "dispBlock" });
+    } else {
+      this.setState({ passwordRequired: "dispNone" });
+    }
+  };
+  inputUsernameChangeHandler = (e) => {
+    this.setState({ username: e.target.value });
+  };
+  inputPasswordChangeHandler = (e) => {
+    this.setState({ password: e.target.value });
+  };
+
   render() {
     return (
       <div>
@@ -19,18 +49,38 @@ class Login extends Component {
             <CardContent>
               <h1>LOGIN</h1>
               <FormControl required className="login-field">
-                <InputLabel htmlFor="username">Username </InputLabel>
-                <Input id="username" type="text" />
+                <InputLabel htmlFor="username">Username</InputLabel>
+                <Input
+                  id="username"
+                  type="text"
+                  username={this.state.username}
+                  onChange={this.inputUsernameChangeHandler}
+                />
+                <FormHelperText className={this.state.usernameRequired}>
+                  <span className="red">Required</span>
+                </FormHelperText>
               </FormControl>
               <br />
               <br />
               <FormControl required className="login-field">
-                <InputLabel htmlFor="password">Password </InputLabel>
-                <Input id="password" type="password" />
+                <InputLabel htmlFor="password">Password</InputLabel>
+                <Input
+                  id="password"
+                  type="password"
+                  username={this.state.password}
+                  onChange={this.inputPasswordChangeHandler}
+                />
+                <FormHelperText className={this.state.passwordRequired}>
+                  <span className="red">Required</span>
+                </FormHelperText>
               </FormControl>
 
               <div className="login-btn">
-                <Button variant="contained" color="primary">
+                <Button
+                  variant="contained"
+                  color="primary"
+                  onClick={this.loginHandler}
+                >
                   login
                 </Button>
               </div>
