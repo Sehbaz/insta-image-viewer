98933344034602ced8e2b05137774113adbb5d60
Sehbaz Rafik <sehbazm@gmail.com>
2021-04-11 11:44:04 +0100

    search field added in header

--- src/common/header/header.js
+++ src/common/header/header.js
@@ -1,8 +1,11 @@
 import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import { withStyles } from "@material-ui/core/styles";
+import TextField from "@material-ui/core/TextField";
 import "../header/header.css";
+
 const { Component } = require("react");
+
 const useStyles = makeStyles((theme) => ({
   root: {
     flexGrow: 1,
@@ -19,7 +22,12 @@ class Header extends Component {
     return (
       <div className="header-container">
         <div className="container-item logo">Image Viewer</div>
-        <div className="container-item"> Logo</div>
+        <div className="container-item home-search">
+          <div>
+            <input type="text" placeholder="Search.." />
+          </div>
+          <div>Profile Logo</div>
+        </div>
       </div>
     );
   }
